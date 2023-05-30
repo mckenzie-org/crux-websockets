c71fb4a93d139b7a66a1f4065e44c82aa706ace7
Akshay Kolte <akshay.kolte@etlok.com>
2023-05-30 14:45:28 -0400

    Websocket Quick Fix

--- src/Facades/SocketRoute.php
+++ src/Facades/SocketRoute.php
@@ -12,6 +12,6 @@ class SocketRoute extends Facade {
      */
     protected static function getFacadeAccessor()
     {
-        return 'websocket.route';
+        return 'websockets.route';
     }
 }
